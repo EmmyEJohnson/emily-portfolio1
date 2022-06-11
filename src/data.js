fb8dd16e514a4e7224043bdd42481789e69d5ba3
Emily Johnson <emelizajo22@gmail.com>
2022-06-11 12:08:21 -0700

    rearrange data file

--- src/data.js
+++ src/data.js
@@ -7,33 +7,33 @@ export const projects = [
     image: "./images/mernStack.png",
     link: "https://reactbootcamp.com",
   },
-
-  {
-    title: "JavaScript",
-    subtitle: "Pure JavaScript",
-    description:
-      "Most recent game built using Javascript and Phaser. Jump around collecting stars and avoiding 'bad gus' and 'bombs' using arrow keys.",
-    image: "./images/htmlcssJS.png",
-    link: "https://github.com/EmmyEJohnson/star-collector",
-  },
-
+  
   {
     title: "The Virtual Stir",
     subtitle: "Express",
     description:
-      "An assigned project to a partner and I from General Assembly. We started to create a virtual bar hopping app during the Covid quarantine.",
+    "An assigned project to a partner and I from General Assembly. We started to create a virtual bar hopping app during the Covid quarantine.",
     image: "./images/express.png",
     link: "https://reactbootcamp.com",
   },
-
+  
   {
     title: "Epic Fails App",
     subtitle: "Python and Django",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./images/pythonDjango.png",
     link: "https://reactbootcamp.com",
   },
+  
+    {
+      title: "JavaScript",
+      subtitle: "Pure JavaScript",
+      description:
+        "Most recent game built using Javascript and Phaser. Jump around collecting stars and avoiding 'bad gus' and 'bombs' using arrow keys.",
+      image: "./images/htmlcssJS.png",
+      link: "https://github.com/EmmyEJohnson/star-collector",
+    },
 ];
 
 export const testimonials = [
